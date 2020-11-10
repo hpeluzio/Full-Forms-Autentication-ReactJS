fcd82f81b16813afdbee1da4d66efcab0ae18d92
Henrique Peluzio <hpeluzio@gmail.com>
2020-11-09 23:12:15 -0300

    Custom hook Erros created

--- src/components/FormularioCadastro/DadosUsuario.jsx
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -1,31 +1,14 @@
 import React, { useState, useContext } from 'react'
 import { TextField, Button } from '@material-ui/core'
 import ValidacoesCadastro from '../../contexts/ValidacoesCadastro'
+import useErros from '../../hooks/useErros'
 
 function DadosUsuario({ aoEnviar }) {
   //
   const [email, setEmail] = useState('')
   const [senha, setSenha] = useState('')
-  const [erros, setErros] = useState({ senha: { valido: true, texto: '' } })
-
   const validacoes = useContext(ValidacoesCadastro)
-
-  function validarCampos(event) {
-    const { name, value } = event.target
-    // console.log(name, ' - ', value)
-    const novoEstado = { ...erros }
-    // console.log(novoEstado)
-    novoEstado[name] = validacoes[name](value)
-
-    setErros(novoEstado)
-  }
-
-  function possoEnviar() {
-    for (let campo in erros) {
-      if (!erros[campo].valido) return false
-    }
-    return true
-  }
+  const [erros, validarCampos, possoEnviar] = useErros(validacoes)
 
   return (
     <form
--- src/contexts/ValidacoesCadastro.js
+++ src/contexts/ValidacoesCadastro.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
-const ValidacoesCadastro = React.createContext()
+const ValidacoesCadastro = React.createContext({
+  cpf: semValidacao,
+  senha: semValidacao,
+  nome: semValidacao,
+})
+
+//Default Provider
+function semValidacao(dados) {
+  return { valido: true, texto: '' }
+}
 
 export default ValidacoesCadastro
